0dff7f36aa037f8657beb8decd13413cf0314fc7
Pranav R Prasad <pranavrp.pr@gmail.com>
2020-10-05 15:37:38 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -4,13 +4,13 @@ public class PrimeExample{
 
   int i,m=0,flag=0;      
 
-  int n=3;//it is the number to be checked    
+  int n=3;//it is the number to be checked  
 
   m=n/2;      
 
   if(n==0||n==1){  
 
-   System.out.println(n+" is not prime number");      
+   System.out.println(n+" is not prime number!");      
 
   }else{  
 
